e4f3486dd629231198a61bf691f52423633a61b4
dosisod <39638017+dosisod@users.noreply.github.com>
2026-01-20 20:26:05 -0800

    Cleanup

--- Libraries/LibJS/Bytecode/ASTCodegen.cpp
+++ Libraries/LibJS/Bytecode/ASTCodegen.cpp
@@ -474,7 +474,6 @@ Bytecode::CodeGenerationErrorOr<Optional<ScopedOperand>> Identifier::generate_by
                 // trigger the TDZ check.
                 generator.emit<Bytecode::Op::Mov>(local, generator.add_constant(js_special_empty_value()));
             }
-            // dbgln("ident emit throw TDZ");
             generator.emit<Bytecode::Op::ThrowIfTDZ>(local);
         }
         return local;
@@ -643,7 +642,6 @@ Bytecode::CodeGenerationErrorOr<Optional<ScopedOperand>> AssignmentExpression::g
                         auto is_initialized = generator.is_local_initialized(identifier.local_index());
                         auto is_lexically_declared = generator.is_local_lexically_declared(identifier.local_index());
                         if (is_lexically_declared && !is_initialized) {
-                            // dbgln("assign emit throw TDZ");
                             generator.emit<Bytecode::Op::ThrowIfTDZ>(generator.local(identifier.local_index()));
                         }
                     }
@@ -1788,7 +1786,6 @@ Bytecode::CodeGenerationErrorOr<Optional<ScopedOperand>> CallExpression::generat
         if (identifier.is_local()) {
             auto local = generator.local(identifier.local_index());
             if (!generator.is_local_initialized(local.operand().index())) {
-                // dbgln("call expr emit throw TDZ");
                 generator.emit<Bytecode::Op::ThrowIfTDZ>(local);
             }
             original_callee = local;
--- Libraries/LibJS/Bytecode/Generator.cpp
+++ Libraries/LibJS/Bytecode/Generator.cpp
@@ -371,32 +371,27 @@ CodeGenerationErrorOr<GC::Ref<Executable>> Generator::compile(VM& vm, ASTNode co
 
                 if (instruction.type() == Instruction::Type::Mov) {
                     auto position = it.offset();
-                    // dbgln("Is at end? {}", it.at_end());
                     ++it;
-                    // dbgln("Is at end? {}", it.at_end());
 
-                    if (it.at_end()) {
-                        // dbgln("oof at end");
-                    } else {
+                    if (!it.at_end()) {
                         auto& next_instruction = const_cast<Instruction&>(*it);
 
                         if (next_instruction.type() == Instruction::Type::Return) {
                             auto& mov = static_cast<Bytecode::Op::Mov const&>(instruction);
                             auto& ret = static_cast<Bytecode::Op::Return const&>(*it);
 
-                            if (mov.dst() == ret.value()) {
-
-                                // OPTIMIZATION: Moved value can be returned directly
-                                Op::Return return_op(mov.src());
-
-                                // dbgln("optimizing return");
-                                bytecode.append(reinterpret_cast<u8 const*>(&return_op), return_op.length());
-                                ++it;
-
+                            // OPTIMIZATION: Moved value is not returned, so move is redundant
+                            // TODO: replace with proper dead store pass
+                            if (mov.dst() != ret.value())
                                 continue;
-                            } else {
-                                // dbgln("wouldve removed MOV");
-                            }
+
+                            // OPTIMIZATION: Moved value can be returned directly
+                            Op::Return return_op(mov.src());
+
+                            bytecode.append(reinterpret_cast<u8 const*>(&return_op), return_op.length());
+                            ++it;
+
+                            continue;
                         }
                     }
 
