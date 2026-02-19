6306082dee24fe0c42e743a90bbf5f2ba0d058a8
dosisod <39638017+dosisod@users.noreply.github.com>
2026-02-18 18:13:19 -0800

    Add initial v2 peephole optimizer:
    This adds some peepholes which speed up the JetStream suite by almost 3x,
    but are causing some SEGILL terminations on 3 tests. Dont know why.

--- Libraries/LibJS/Bytecode/Generator.cpp
+++ Libraries/LibJS/Bytecode/Generator.cpp
@@ -404,6 +404,41 @@ GC::Ref<Executable> Generator::compile(VM& vm, ASTNode const& node, FunctionKind
 
                         continue;
                     }
+
+                    if (auto next = it.peek(Instruction::Type::Mov); next.has_value()) {
+                        auto& mov = static_cast<Bytecode::Op::Mov const&>(instruction);
+                        auto& mov_next = static_cast<Bytecode::Op::Mov const&>(*next);
+
+                        // OPTIMIZATION: move destination will immediately be overriden, skip emit
+                        if (mov.dst() == mov_next.dst()) {
+                            bytecode.append(reinterpret_cast<u8 const*>(next.ptr()), next->length());
+                            ++it;
+
+                            continue;
+                        }
+
+                        bytecode.append(reinterpret_cast<u8 const*>(&instruction), instruction.length());
+                        continue;
+                    }
+                }
+
+                if (instruction.type() == Instruction::Type::ToInt32) {
+                    if (auto next = it.peek(Instruction::Type::Mov); next.has_value()) {
+                        auto& to_i32 = static_cast<Bytecode::Op::ToInt32 const&>(instruction);
+                        auto& mov = static_cast<Bytecode::Op::Mov const&>(*next);
+
+                        // OPTIMIZATION: store to same register instead of doing move
+                        if (to_i32.value() == mov.dst()) {
+                            Op::ToInt32 to_i32_op(to_i32.value(), to_i32.value());
+                            bytecode.append(reinterpret_cast<u8 const*>(&to_i32_op), to_i32_op.length());
+                            ++it;
+
+                            continue;
+                        }
+
+                        bytecode.append(reinterpret_cast<u8 const*>(&instruction), instruction.length());
+                        continue;
+                    }
                 }
 
                 if (instruction.type() == Instruction::Type::Jump) {
